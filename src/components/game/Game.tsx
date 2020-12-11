385c08086d439a9d0547a5ca9e4a7068e37d50bf
jensm <meire.jens@gmail.com>
2020-12-11 01:09:00 +0100

    Keyboard support

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, {useCallback} from "react";
 import GridComponent from "components/grid/Grid";
+import {useGame} from "infrastructure/game/GameContext";
+import useEventListener from "infrastructure/utils/useEventListener";
 
 const GameComponent = (): React.ReactElement => {
+  const {moveLeft, moveDown, moveRight, rotateRight} = useGame()
+
+  const handleKeyDown = useCallback((event: Event): void => {
+    const keyboardEvent = event as KeyboardEvent;
+    if (!keyboardEvent || !keyboardEvent.code) return
+    switch (keyboardEvent.code.toLowerCase()) {
+      case "arrowup":
+        rotateRight();
+        break;
+      case "arrowdown":
+        moveDown();
+        break;
+      case "arrowleft":
+        moveLeft();
+        break;
+      case "arrowright":
+        moveRight();
+        break;
+    }
+  }, [moveLeft, moveDown, moveRight, rotateRight])
+
+  useEventListener("keydown", handleKeyDown);
+
   return <div>
     <GridComponent/>
   </div>
---
+++ src/infrastructure/utils/useEventListener.ts
@@ -0,0 +1,29 @@
+import {useRef, useEffect, RefObject} from 'react'
+
+function useEventListener<T extends HTMLElement = HTMLDivElement>(
+  eventName: string,
+  handler: Function,
+  element?: RefObject<T>,
+) {
+  const savedHandler = useRef<Function>()
+  useEffect(() => {
+    const targetElement: T | Window = element?.current || window
+    if (!(targetElement && targetElement.addEventListener)) {
+      return
+    }
+    if (savedHandler.current !== handler) {
+      savedHandler.current = handler
+    }
+    const eventListener = (event: Event) => {
+      if (!!savedHandler?.current) {
+        savedHandler.current(event)
+      }
+    }
+    targetElement.addEventListener(eventName, eventListener)
+    return () => {
+      targetElement.removeEventListener(eventName, eventListener)
+    }
+  }, [eventName, element, handler])
+}
+
+export default useEventListener
